faff677150b0d6239d4ca1cff78a66772ba9d27d
Jilpung <86465970+Jilpung@users.noreply.github.com>
2022-11-18 13:47:24 +0900

    SecurityConfig 완성 > member, board controller url이 섞여있어서 수정필요

--- src/main/java/com/josh/board/joshboard/domain/repository/MemberRepository.java
+++ src/main/java/com/josh/board/joshboard/domain/repository/MemberRepository.java
@@ -1,4 +1,11 @@
 package com.josh.board.joshboard.domain.repository;
 
-public interface MemberRepository {
+import com.josh.board.joshboard.domain.entity.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+    Optional<MemberEntity> findByEmail(String userEmail);
+
 }
--- src/main/java/com/josh/board/joshboard/dto/MemberDto.java
+++ src/main/java/com/josh/board/joshboard/dto/MemberDto.java
@@ -1,4 +1,33 @@
 package com.josh.board.joshboard.dto;
 
+import com.josh.board.joshboard.domain.entity.MemberEntity;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class MemberDto {
+    private Long id;
+    private String email;
+    private String password;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+
+    public MemberEntity toEntity() {
+        return MemberEntity.builder()
+                .id(id)
+                .email(email)
+                .password(password)
+                .build();
+    }
+
+    @Builder
+    public MemberDto(Long id, String email, String password){
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
 }
